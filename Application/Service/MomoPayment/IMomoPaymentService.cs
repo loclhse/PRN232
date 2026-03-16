283c0de07be054b0862affe6775599ba1dfaa091
But <hoahuynh882003@gmail.com>
2026-03-16 16:21:32 +0700

    [UPDATE] Authorize in Controller

--- Application/Service/MomoPayment/IMomoPaymentService.cs
+++ Application/Service/MomoPayment/IMomoPaymentService.cs
@@ -1,12 +1,12 @@
-﻿using Application.DTOs.Response.MomoPayment;
-using Application.DTOs.Request.MomoPayment;
+﻿using Application.DTOs.Request.MomoPayment;
+using Application.DTOs.Response.MomoPayment;
 
 namespace Application.Service.MomoPayment
 {
     public interface IMomoPaymentService
     {
-        Task<MomoPaymentResponse> CreatePaymentAsync(Guid orderId, string? orderInfo = null);
-        Task<MomoPaymentStatusResponse> QueryPaymentStatusAsync(Guid orderId);
+        Task<MomoPaymentResponse> CreatePaymentAsync(Guid orderId, Guid currentUserId, string? orderInfo = null);
+        Task<MomoPaymentStatusResponse> QueryPaymentStatusAsync(Guid orderId, Guid currentUserId);
         Task HandleIpnAsync(MomoIpnRequest request);
     }
 }
